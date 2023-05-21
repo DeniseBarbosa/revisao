9136e2fd9620cba683d7b194f46b813e741cea39
DeniseBarbosa <deniseluvoir@gmail.com>
2023-05-21 08:54:50 -0300

    undefined e null

---
+++ undefined-null.js
@@ -0,0 +1,8 @@
+let naoDeclararValor;
+
+console.log(`tipo ${typeof  naoDeclararValor}`);
+console.log(naoDeclararValor);
+naoDeclararValor =10;
+console.log(naoDeclararValor);
+let nulo = null;
+console.log(`qual o tipo? ${typeof nulo}`);
