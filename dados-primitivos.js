7ed928f7c07ad434d8b4be3c747600ae10028be1
DeniseBarbosa <deniseluvoir@gmail.com>
2023-05-21 08:40:25 -0300

    dados primitivos

---
+++ dados-primitivos.js
@@ -0,0 +1,15 @@
+//string, number (int, floats), boolean
+
+let numero = 10;
+let numero1 = 10.2;
+let nome = "Denise";
+let estaLigado = false;
+const frase = `Eu amo gatitos(é uma template literal)`;
+const pessoa = "Denise";
+const apresentacao = `Meu nome é ${pessoa}`;
+console.log(numero);
+console.log(numero1);
+console.log(nome);
+console.log(estaLigado);
+console.log(frase);
+console.log(apresentacao);
