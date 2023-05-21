5eca7204f6b55283e4aec3aa1ad572a1b5dacaf8
DeniseBarbosa <deniseluvoir@gmail.com>
2023-05-21 08:26:18 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+// var -> ES5 -> cross-browser
+// let e const -> ES2015
+// para stringg -> tanto faz "" ou ''
+
+let teste = "minha sring";
+teste = 10;
+const teste2 = 100;
+let numero;
+numero = 50;
+console.log(numero);
+console.log(teste2);
+console.log(teste);
