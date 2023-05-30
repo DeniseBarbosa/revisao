dc37c0ec5605f73df2ffea24afb1eef81ebe4bb0
DeniseBarbosa <deniseluvoir@gmail.com>
2023-05-30 18:04:11 -0300

    conversão

---
+++ conversao.js
@@ -0,0 +1,11 @@
+let n1 = 10;
+let n2 = "10";
+let calcular = n1+n2;
+let converter = Number(n2);
+
+console.log(calcular);
+console.log(n1 + n2);
+console.log(n1 * n2);
+console.log(`${typeof n2}`);
+console.log(`${typeof converter}`);
+
