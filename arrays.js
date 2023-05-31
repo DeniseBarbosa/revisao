72b35c5bcb54261778338d3849879efa59321b79
DeniseBarbosa <deniseluvoir@gmail.com>
2023-05-31 20:12:03 -0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,17 @@
+const arrays = new Array()
+console.log(typeof arrays);
+console.log(  arrays);
+const arrays1 = new Array(true, "denise", 25,
+new Array(false,"casa", 1.2)
+);
+console.log(arrays1);
+const animais = ["cobra","largato","tigre","gato","cachorro"];
+console.log(animais[0]);
+console.log(animais[3]);
+console.log(animais.length);
+animais[3] = 10;
+console.log(animais);
+
+let nomes = ["Jão","Vitor","Cintia", [1,2,3]];
+nomes.push("a","b");
+console.log(nomes);
